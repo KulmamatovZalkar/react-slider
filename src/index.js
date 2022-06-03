f561e4ed1c19235478953ee601b99089173ea0de
Kulmamatov Zalkar <kulmamatov.zalkar@gmail.com>
2022-06-03 20:32:31 +0600

    Update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./style.scss";
+import "./scss/style.scss";
 
 import slides from "./slider/slides";
 import CitiesSliderComponent from "./slider/CitiesSliderComponent";
